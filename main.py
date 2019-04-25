fdcd48d9f66a79fcd2e9d63d3ded8bc2568d3f59
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-04-25 16:20:16 +0900

    ちょっと加筆した。

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ Last Update :
 
 
 from torch.utils.data import Dataset
+from torchvision import transforms
 import os
 import sys
 import pandas as pd
+import cv2
+import numpy as np
 
 LABEL_IDX = 1
 IMG_IDX = 2
 
 
 class MyDataset(Dataset):
+    '''
+    dataset class
+    '''
 
     def __init__(self, csv_file_path, root_dir, transform=None):
         # pandasでcsvデータの読み出し
@@ -36,9 +42,30 @@ class MyDataset(Dataset):
         img_name = os.path.join(self.root_dir, 'classification-of-handwritten-letters',
                 'letters2', self.image_dataframe.iat[idx, IMG_IDX])
         # 画像の読み込み
-        image = io.imread(img_name)
+        # image = io.imread(img_name)
+        image = cv2.imread(img_name)
         # 画像へ処理を加える
         if self.transform:
             image = self.transform(image)
 
         return image, label
+
+
+class MyNormalize:
+    '''
+    self define normalize
+    '''
+
+    def __call__(self, image):
+        shape = image.shape
+        image = (image - np.mean(image)) / np.std(image) * 16 + 64
+        return image
+
+
+# file path settings
+
+# create Dataset
+imgDataset = MyDataset(input_file_path, ROOT_DIR, transform=transforms.Compose([
+    transforms.ToTensor(),
+    MyNormalize()
+    ]))
