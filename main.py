42bc21b99db2772a571f7c93e51e545ffcff8c29
Yuki Kumon <yuki-kumon@g.ecc.u-tokyo.ac.jp>
2019-04-26 09:32:24 +0900

    ネットワークの定義を加筆。

--- main.py
+++ main.py
@@ -5,13 +5,16 @@
 Author :
     Yuki Kumon
 Last Update :
-    2019-04-25
+    2019-04-26
 """
 
 
 from torch.utils.data import Dataset
 from torchvision import transforms
-import torch
+import torch.nn as nn  # ネットワーク構築用
+import torch.optim as optim  # 最適化関数
+import torch.nn.functional as F  # ネットワーク用の様々な関数
+import torch.utils.data  # データセット読み込み関連
 import os
 import sys
 import pandas as pd
@@ -20,6 +23,10 @@ import numpy as np
 
 LABEL_IDX = 1
 IMG_IDX = 2
+
+# file path settings
+input_file_path = './data/letters2/letters2.csv'
+ROOT_DIR = './data/letters2/'
 
 
 class MyDataset(Dataset):
@@ -58,14 +65,34 @@ class MyNormalize:
     '''
 
     def __call__(self, image):
+        '''
+        write my transform here
+        '''
         shape = image.shape
         image = (image - np.mean(image)) / np.std(image) * 16 + 64
         return image
 
 
-# file path settings
-input_file_path = './data/letters2/letters2.csv'
-ROOT_DIR = './data/letters2/'
+# define my network
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # 畳み込み層(サンプル数、チャネル数、窓のサイズ)
+        self.conv1 = nn.Conv2d(4, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(500, 100)
+        self.fc2 = nn.Linear(100, 34)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 500)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
 
 # create Dataset
 imgDataset = MyDataset(input_file_path, ROOT_DIR, transform=transforms.Compose([
@@ -82,7 +109,6 @@ train_data, test_data = torch.utils.data.random_split(imgDataset, [train_size, t
 # create dataloader
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=100, shuffle=True)
-
 
 
 image_test = cv2.imread('./data/letters2/33_223.png')
